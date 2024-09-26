7da35c5ab3d5d53d45d8045abc75092e1fe2de4c
Tobi <streckel@gmx.de>
2024-09-26 18:53:45 +0200

    Update FontIconPicker.php

--- src/FontIconPicker/FontIconPicker.php
+++ src/FontIconPicker/FontIconPicker.php
@@ -1,58 +1,49 @@
 <?php
-
 namespace strtob\yii2WidgetToolkit\FontIconPicker;
 
-use Yii;
-use yii\base\Widget;
-use yii\helpers\Html;
+use yii\web\AssetBundle;
 
-class FontIconPicker extends Widget
+class FontIconPickerAsset extends AssetBundle
 {
-    public $model; 
-    public $attribute; 
-    public $iconList = []; 
-    public $options = []; 
+    public $sourcePath = '@vendor/strtob/yii2-widget-toolkit/src/FontIconPicker/node_modules/@fonticonpicker/fonticonpicker/dist';
+    
+    public $css = [      
+        'css/base/jquery.fonticonpicker.min.css',
+        'css/themes/bootstrap-theme/jquery.fonticonpicker.bootstrap.min.css',
+    ];
+    
+    public $js = [
+        'js/jquery.fonticonpicker.min.js',
+    ];
 
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+
+    public $publishOptions = [
+        'forceCopy' => YII_DEBUG, // Enable force copy in debug mode
+    ];
+
+    // Add the fonts to the asset bundle
+    public $fonts = [
+        'fonts/iconpicker.eot',
+        'fonts/iconpicker.woff',
+        'fonts/iconpicker.ttf',
+        'fonts/iconpicker.svg',
+    ];
+    
     public function init()
     {
         parent::init();
-        
-        if (empty($this->iconList)) {
-            $this->iconList = ['fa fa-user', 'fa fa-home', 'fa fa-cog']; 
+        $this->publishFonts();
+    }
+
+    protected function publishFonts()
+    {
+        // This method publishes the fonts
+        foreach ($this->fonts as $font) {
+            $this->css[] = $font; // You may use $this->css or $this->js depending on the structure.
         }
-
-
-	
-    	if (!isset($this->options['id'])) {
-        	$this->options['id'] = $this->getId();  
-    	}
-
-    }
-
-    public function run()
-    {
-        $icon = Html::activeTextInput($this->model, $this->attribute, $this->options);
-        return $icon . $this->registerScripts();
-    }
-
-    protected function registerScripts()
-    {
-        $view = $this->getView();
-        FontIconPickerAsset::register($view);
-
-        $js = <<<JS
-        $(document).ready(function() {
-            $('#{$this->options['id']}').fontIconPicker({
-                icons: {$this->generateIconList()}
-            });
-        });
-        JS;
-
-        $view->registerJs($js);
-    }
-
-    protected function generateIconList()
-    {
-        return json_encode($this->iconList);
     }
 }
