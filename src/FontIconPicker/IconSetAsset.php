df3c174047fecae44dbfafe97b75e10432412ad1
Tobias Streckel <ts@finance-re.de>
2024-09-26 19:33:34 +0200

    IconSet Asset

---
+++ src/FontIconPicker/IconSetAsset.php
@@ -0,0 +1,23 @@
+<?php
+namespace strtob\yii2WidgetToolkit\FontIconPicker;
+
+use yii\web\AssetBundle;
+
+class FontIconPickerAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/strtob/yii2-widget-toolkit/src/FontIconPicker';
+    
+    public $js = [
+        'js/IconSet.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+
+    public $publishOptions = [
+        'forceCopy' => YII_DEBUG, // Enable force copy in debug mode
+    ];
+
+}
