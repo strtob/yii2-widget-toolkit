92db33e5e6bf0c982041b92a7f3e2b70c437e36c
Tobi <streckel@gmx.de>
2024-09-26 18:53:28 +0200

    Update FontIconPickerAsset.php

--- src/FontIconPicker/FontIconPickerAsset.php
+++ src/FontIconPicker/FontIconPickerAsset.php
@@ -1,19 +1,46 @@
 <?php
+
 namespace strtob\yii2WidgetToolkit\FontIconPicker;
 
-use yii\web\AssetBundle;
+use Yii;
 
-class FontIconPickerAsset extends AssetBundle
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\bootstrap5\InputWidget;
+
+class FontIconPicker extends InputWidget
 {
-    public $sourcePath = '@bower/fontawesome-iconpicker/dist'; 
-    public $css = [
-        'css/fontawesome-iconpicker.min.css',
-    ];
-    public $js = [
-        'js/fontawesome-iconpicker.min.js',
-    ];
-    public $depends = [
-        'yii\web\JqueryAsset',
-        'yii\bootstrap5\BootstrapAsset', 
-    ];
+
+    public $clientOptions = [];
+
+    public function run()
+    {
+        if ($this->hasModel()) {
+            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textInput($this->name, $this->value, $this->options);
+        }
+        $this->registerClientScript();
+    }
+
+    /**
+     * Publish resource
+     */
+    protected function registerClientScript()
+    {
+        $js = [];
+        $view = $this->getView();
+        FontIconPickerAsset::register($view);
+        $id = $this->options['id'];
+        $options = Json::encode($this->clientOptions);
+ 
+        $js[] = "$(document).ready(function() {
+            $('#{$id}').fontIconPicker({
+               
+                theme: 'fip-darkgrey'
+            });
+        });";
+
+        $view->registerJs(implode("\n", $js));
+    }
 }
