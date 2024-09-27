a15e422371008a3b5583d04bff68d24be9b0a482
Tobias Streckel <ts@finance-re.de>
2024-09-27 20:37:35 +0200

    Select2Country Assets

---
+++ src/Select2Country/Select2CountryAsset.php
@@ -0,0 +1,13 @@
+namespace strtob\yii2WidgetToolkit\Select2Country;
+
+use yii\web\AssetBundle;
+
+class Select2CountryAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/strtob/yii2-widget-toolkit/src/Select2Country/flags';
+    public $css = [];  // Include any necessary CSS files
+    public $js = [];   // Include any necessary JS files
+    public $depends = [
+        // Include any dependencies like jQuery, Bootstrap, etc.
+    ];
+}
